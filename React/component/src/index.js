2044f528c87dee8133af4396c132e61611c3e169
Lee Jae Hun <wogns5206@naver.com>
2019-10-28 14:28:55 +0900

    React-Basic

--- React/component/src/index.js
+++ React/component/src/index.js
@@ -8,43 +8,64 @@ import CommentDetail from './components/CommentDetail';
 
 import faker from 'faker';
 
-const App = () => {
 
-    return (
-        <Fragment>
-            <Segment>
-                <div className="ui icon header">
-                    <i className="pdf file outline icon">No Document</i>
+class App extends React.Component {
+
+    state = {
+        comments: [],
+        lorem: faker.lorem.paragraph()
+    };
+
+    handleAddComment = () => {
+        const newComment = {
+            author: faker.name.firstName(),
+            time: faker.date.recent().toLocaleString(),
+            body: faker.lorem.sentence(),
+            avatar: faker.image.avatar()
+        };
+        this.setState({ comments: [newComment, ...this.state.comments] });
+    };
+
+    render() {
+
+        return (
+            <Fragment>
+
+                <Segment>
+                    <div className="ui icon header">
+                        <i className="pdf file outline icon">No Document</i>
+                    </div>
+                    <div className="ui primary button">Add Document</div>
+                </Segment>
+
+                <Segment>
+                    <h4 className="ui header">For your infomation!</h4>
+                    <p>{this.state.lorem}</p>
+                </Segment>
+
+                <Message header='회원 약관 변경' body='약관이 변경되었습니다. 동의하실거죠?' />
+                <div className="ui container comments">
+                    <button className="ui primary button" onClick={this.handleAddComment}>코멘트 추가하기</button>
+                    <ApprovalCard>
+                        <h4>저는 오늘 새벽까지 복습할거에요.</h4>
+                        <p>빡공각?</p>
+                    </ApprovalCard>
+
+                    {this.state.comments.map(comment => {
+                        return (
+                            <ApprovalCard>
+                                <CommentDetail {...comment} />
+                            </ApprovalCard>
+                        );
+                    })}
+
                 </div>
-                <div className="ui primary button">Add Document</div>
-            </Segment>
 
-            <Segment>
-                <h4 className="ui header">For your infomation!</h4>
-                <p>{faker.lorem.paragraph()}</p>
-            </Segment>
-
-            <Message header='회원 약관 변경' body='약관이 변경되었습니다. 동의하실거죠?' />
-            <div className="ui container comments">
-                <ApprovalCard>
-                    <h4>저는 오늘 새벽까지 복습할거에요.</h4>
-                    <p>빡공각?</p>
-                </ApprovalCard>
-
-                <ApprovalCard>
-                    <CommentDetail
-                        author={faker.name.firstName()}
-                        time={faker.date.recent().toLocaleString()}
-                        body={faker.lorem.sentence()}
-                        avatar={faker.image.avatar()}
-                    />
-                </ApprovalCard>
-            </div>
-
-        </Fragment>
-    )
-
+            </Fragment>
+        )
+    }
 }
+
 
 
 ReactDOM.render(<App />, document.querySelector('#root'));
