21112c01148fa1c9fd6775d5830b400418149d60
Lee Jae Hun <wogns5206@naver.com>
2019-10-30 13:45:45 +0900

    season-checker basic

---
+++ React/season-checker/src/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Spinner(props) {
+    return (
+        <div className="spinner ui active dimmer">
+            <div className="ui big text loader">
+                {props.message}
+            </div>
+        </div>
+    );
+}
